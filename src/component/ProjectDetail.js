f0950e97c909cab727beda9d7355af46ccf462e6
Adrien14235 <164215405+Adrien14235@users.noreply.github.com>
2024-12-17 16:49:43 +0100

    .zip ajouter

--- src/component/ProjectDetail.js
+++ src/component/ProjectDetail.js
@@ -19,14 +19,14 @@ import img16 from '../projets/gamehub1.png';
 
 import doc1 from '../projets/cdc_biblio.pdf'
 import zip1 from '../projets/respirestatetug03.zip';
-import doc6 from '../projets/gamehub.pdf';
+import zip2 from '../projets/gamehub.zip';
 
 const projects = [
   { id: 1, title: 'RespireStatEtu', images: [img1, img2, img3], description: 'En tant que développeur nous devions recensés les polluants au abords des crèches et des écoles en Île de France ', file: zip1  },
   { id: 3, title: 'La Maison Jungle', images: [img7], description: "En arrivant dans mon stage j'ai du apprendre de nouveau language telle que React.", },
   { id: 4, title: 'Liste de Film', images: [img8, img9, img10], description: "Le projet dans lequel je suis arrivé pour mon stage fonctionnait autour d'API.",  },
   { id: 5, title: 'Bibliothèque Virtuel V2', images: [img11, img12, img13], description: "Le projet Bibliothèque Virtuel V2 est un projet consistant à améliorer la précédente bibliothèque.",file: doc1 },
-  { id: 6, title: 'Gamehub', images: [img16, img14, img15], description: "", file: doc6 },
+  { id: 6, title: 'Gamehub', images: [img16, img14, img15], description: "", file: zip2 },
 ];
 
 const ProjectDetails = () => {
